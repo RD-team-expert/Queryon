54e660b6672724ea896f584069f16f7072840b6b
ShahdAboLatif <shahdabolatif@gmail.com>
2025-07-12 12:22:13 -0400

    fixed validation for health plan

--- database/migrations/2025_07_09_143343_create_health_plan_table.php
+++ database/migrations/2025_07_09_143343_create_health_plan_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id(); // Auto-incrementing primary key
             $table->string('first_name')->nullable(); // VARCHAR for first name, can be null
             $table->string('last_name')->nullable(); // VARCHAR for last name, can be null
-            $table->string('email')->unique()->nullable(); // VARCHAR for email, unique and can be null
+            $table->string('email')->nullable(); // VARCHAR for email, unique and can be null
             $table->string('store')->nullable(); // VARCHAR for store, can be null
             $table->string('onboarding_offboarding')->nullable(); // VARCHAR for onboarding/offboarding status, can be null
             $table->date('working_start_date')->nullable(); // DATE for working start date, can be null
