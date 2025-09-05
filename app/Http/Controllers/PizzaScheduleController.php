ffea217d9e98f7e11710bdeed3629479151654ef
ShahdAboLatif <shahdabolatif@gmail.com>
2025-09-05 07:47:30 -0400

    schedule update

--- app/Http/Controllers/PizzaScheduleController.php
+++ app/Http/Controllers/PizzaScheduleController.php
@@ -9,6 +9,7 @@ use App\Models\WeeklyScheduleSummary;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
 
+
 class PizzaScheduleController extends Controller
 {
     public function store(Request $request)
