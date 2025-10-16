f181890eef8452a8640970509c7dfba006b19d12
ShahdAboLatif <shahdabolatif@gmail.com>
2025-10-16 12:46:46 -0400

    new update for hiring

--- app/Models/Hiring/HiringRequest.php
+++ app/Models/Hiring/HiringRequest.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class HiringRequest extends Model
 {
     protected $table = 'hiring_requests';
-    public $timestamps = false;
+    public $timestamps = true;
 
     protected $fillable = [
         'first_name',
